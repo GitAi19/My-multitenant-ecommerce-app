e8880d2013f7d7e8a374d31088f96d88f933dc62
GitAi19 <sakshiaish2003@gmail.com>
2026-05-27 16:54:00 +0530

    1_Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <div>
+        {/* <Button variant="gap">
+          Heloooooo
+        </Button> */}
+        <p className="text-rose-500">
+          HEY THERE
+        </p>
+      </div>
+  );
+};
